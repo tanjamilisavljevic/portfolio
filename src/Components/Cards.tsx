6a3ec771a848928a8053e79c8d728428c7af9de2
tanjamilisavljevic <70627722+tanjamilisavljevic@users.noreply.github.com>
2021-09-20 16:20:37 +0200

    Add connect links and animation to picture

--- src/Components/Cards.tsx
+++ src/Components/Cards.tsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import BirthdayGirl from "../birthday-girl.png";
-import BirthdayGirl2 from "../birthday-girl2.png";
 import BirthdayCard from "../birthday-card.png";
 import Phone from "../phone.png";
 
@@ -13,8 +12,8 @@ function Cards() {
 
                 <div className='card' id='card1'>
                     <a id='card1'>
-                    <h4> Get to know me here </h4>
-                    <p> This is dummy text. This is dummy text. This is dummy text.</p>
+                        <h4> Get to know me </h4>
+                        <p> This is dummy text. This is dummy text. This is dummy text.</p>
                     </a>
                 </div>
 
@@ -22,8 +21,8 @@ function Cards() {
 
                 <div className='card' id='card2'>
                     <a id='card2'>
-                    <h4> See my work here </h4>
-                    <p> This is dummy text. This is dummy text. This is dummy text.</p>
+                        <h4> See my work here </h4>
+                        <p> This is dummy text. This is dummy text. This is dummy text.</p>
                     </a>
                 </div>
 
@@ -32,12 +31,18 @@ function Cards() {
 
                 <div className='card' id='card3'>
                     <a id='card3'>
-                    <h4> Let's get in touch! </h4>
-                    <p> This is dummy text. This is dummy text. This is dummy text.</p>
+                        <h4> Let's get in touch! </h4>
+                        <p> Connect with me on
+                            <a className='contactLink' href='https://www.linkedin.com/in/tanja-milisavljevic-5444a8212/'> LinkedIn</a>
+                            <br/>OR
+                            <br/> <a className='contactLink' href="mailto: tanjamilisavljevic7@gmail.com"   target="_blank">email me</a>
+                            <br/>OR
+                            <br/><a className='contactLink' href="tel:+32456147888"> call me </a> during office hours!
+                        </p>
                     </a>
                 </div>
 
-                <img className='rightImg' src={Phone}/>
+                <img className='rightImg' id='phone' src={Phone}/>
 
             </div>
         </div>
